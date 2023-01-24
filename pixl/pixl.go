601e65dc5b98492ca8a242a8bd74f09d8627aa50
aibenStunner <ebenezergadri99@gmail.com>
2023-01-24 18:12:23 +0000

    feat: rendered pixel renderer

--- pixl/pixl.go
+++ pixl/pixl.go
@@ -3,8 +3,10 @@ package main
 import (
 	"image/color"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"thegadri.io/pixl/apptype"
+	"thegadri.io/pixl/pxcanvas"
 	"thegadri.io/pixl/swatch"
 	"thegadri.io/pixl/ui"
 )
@@ -18,10 +20,21 @@ func main() {
 		SwatchSelected: 0,
 	}
 
+	pixlCanvasConfig := apptype.PxCanvasConfig{
+		DrawingArea:  fyne.NewSize(600, 600),
+		CanvasOffset: fyne.NewPos(0, 0),
+		PxRow:        10,
+		PxCols:       10,
+		PxSize:       30,
+	}
+
+	pixlCanvas := pxcanvas.NewPxCanvas(&state, pixlCanvasConfig)
+
 	appInit := ui.AppInit{
-		PixlWindow: pixlWindow,
-		State:      &state,
-		Swatches:   make([]*swatch.Swatch, 0, 64),
+		PixelCanvas: pixlCanvas,
+		PixlWindow:  pixlWindow,
+		State:       &state,
+		Swatches:    make([]*swatch.Swatch, 0, 64),
 	}
 
 	ui.Setup(&appInit)
--- ui/types.go
+++ ui/types.go
@@ -3,11 +3,13 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"thegadri.io/pixl/apptype"
+	"thegadri.io/pixl/pxcanvas"
 	"thegadri.io/pixl/swatch"
 )
 
 type AppInit struct {
-	PixlWindow fyne.Window
-	State      *apptype.State
-	Swatches   []*swatch.Swatch
+	PixelCanvas *pxcanvas.PXCanvas
+	PixlWindow  fyne.Window
+	State       *apptype.State
+	Swatches    []*swatch.Swatch
 }
